a82eca19e59bc2879027d6e6c6f2376df6191fda
sitizakiyah <sitizakiyah235@gmail.com>
2026-05-08 23:45:24 +0700

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
-
-Route::get('login', function () {
-    return view('login');
-})->name('login');
 
 Route::get('checkout', function () {
     return view('checkout');
@@ -17,3 +14,15 @@ Route::get('checkout', function () {
 Route::get('success-checkout', function () {
     return view('success_checkout');
 })->name('success-checkout');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
